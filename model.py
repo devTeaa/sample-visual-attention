f321ddc62d3fc7e145b25c4720b7a2b5c12f9b23
devTeaa <devtea@protonmail.com>
2018-11-17 10:51:40 +0700

    add test code

--- model.py
+++ model.py
@@ -72,3 +72,16 @@ for epoch in range(2):  # loop over the dataset multiple times
             running_loss = 0.0
 
 print('Finished Training')
+
+correct = 0
+total = 0
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        outputs = net(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print('Accuracy of the network on the 10000 test images: %d %%' % (
+    100 * correct / total))
